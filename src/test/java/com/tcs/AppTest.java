1bfa2249c73490e431bc9c5e5f098a6c50c89749
Byron Lopez <brlopezc@pichincha.com>
2025-04-01 00:19:18 -0500

    Run Chrome

--- src/test/java/com/tcs/AppTest.java
+++ src/test/java/com/tcs/AppTest.java
@@ -3,7 +3,7 @@ package com.tcs;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
@@ -11,7 +11,7 @@ public class AppTest {
     public static void main(String[] args) {
 
 
-        WebDriver driver = new SafariDriver();
+        WebDriver driver = new ChromeDriver();
 
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
