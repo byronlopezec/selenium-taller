3025806f989fb0ac6dfe39b8dd5078f4c77c970a
brlopezc <brlopezc@pichincha.com>
2025-03-31 22:04:02 -0500

    start proyect

--- src/test/java/com/tcs/AppTest.java
+++ src/test/java/com/tcs/AppTest.java
@@ -1,2 +1,36 @@
-package com.tcs;public class AppTest {
+package com.tcs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class AppTest {
+    public static void main(String[] args) {
+
+
+        WebDriver driver = new SafariDriver();
+
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
+
+        driver.quit();
+
+    }
+
 }
